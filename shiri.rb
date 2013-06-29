9b7cbd77df76c25a6db99f7736a9c0a15cb096fd
nitazou <nitazou@gmail.com>
2013-06-30 02:28:24 +0900

    'jquery'

--- shiri.rb
+++ shiri.rb
@@ -1,6 +1,21 @@
 require "rubygems"
 require "sinatra"
-
+require './syori/shiritori.rb'
+output=Shiritori.new
+ 
 get "/" do
-  "Hello world北ーーーーーー( ;∀;)これは動くか"
+  html :aa
 end
+
+get '/shiritori_process' do
+
+ $input_key=params['key']
+ return output.syori
+ 
+end
+
+
+def html(view)
+  File.read(File.join('public', "#{view.to_s}.html"))
+end
+
---
+++ syori/shiritori.rb
@@ -0,0 +1,8 @@
+class Shiritori
+	def syori
+		output1=$input_key+",日本語"
+		return output1
+	end
+end
+
+
